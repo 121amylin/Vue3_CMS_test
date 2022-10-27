c2811840fccd460f1800a4d7c28d66901c7fa74e
amy <amy@cy-tech.com>
2022-10-27 17:17:20 +0800

    前端分頁

--- CMS_01/src/views/manage/newsManage.vue
+++ CMS_01/src/views/manage/newsManage.vue
@@ -1,3 +1,44 @@
-<template>
+<!-- <template>
    newsManage
+</template> -->
+
+
+<template>
+   {{currentPage1}}
+
+
+   <div class="demo-pagination-block">
+      <div class="demonstration">Total item count</div>
+      <el-pagination v-model:currentPage="currentPage1" :page-size="100" :small="small" :disabled="disabled"
+         :background="background" layout="total, prev, pager, next" :total="1000" @size-change="handleSizeChange"
+         @current-change="handleCurrentChange" />
+   </div>
 </template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const currentPage1 = ref(5)
+
+const small = ref(false)
+const background = ref(false)
+const disabled = ref(false)
+
+const handleSizeChange = (val) => {
+   console.log(`${val} items per page`)
+}
+const handleCurrentChange = (val) => {
+   currentPage1.value=val
+   console.log(`current page: ${val}`)
+}
+</script>
+ 
+<style scoped>
+.demo-pagination-block+.demo-pagination-block {
+   margin-top: 10px;
+}
+
+.demo-pagination-block .demonstration {
+   margin-bottom: 16px;
+}
+</style>
+ 
--- CMS_01/src/views/manage/userManage.vue
+++ CMS_01/src/views/manage/userManage.vue
@@ -1,17 +1,14 @@
 <template>
-    <!-- {{ userList }} -->
+    <div v-if="userList">
+        <el-table :data="newUserList[currentPage]">
+            <el-table-column prop="id" label="Id" />
+            <el-table-column prop="name" label="Name" />
+            <el-table-column prop="email" label="Email" />
+        </el-table>
+    </div>
 
-    <!-- {{ newUserList }} -->
-
-    <!-- {{tempArr}} -->
-    <el-table :data="userList">
-        <el-table-column prop="id" label="Id" />
-        <el-table-column prop="name" label="Name" />
-        <el-table-column prop="email" label="Email" />
-    </el-table>
-
-
-    <el-pagination background layout="prev, pager, next" :total="totalNumber" />
+    <el-pagination background layout="prev, pager, next" :total="totalNumber" v-model='currentPage'
+        @current-change="handleCurrentChange" />
 </template>
 
 
@@ -20,27 +17,31 @@ import { UserAPI } from '../../API/userAPI'
 import { onMounted, ref, computed } from 'vue'
 const userList = ref()
 const totalNumber = ref()
+const newUserList = ref()
+const currentPage = ref(0)
 const userAPI = new UserAPI()
 
-// const newUserList = computed(() => {
-    let tempArr = []
-    let count = Math.ceil((totalNumber.length) * 1 / 10)
-    for (let i = 0; i < 50; i++) {
-        console.log(i)
-        // tempArr.push(userList.value.splice(0, 10))
-    }
-    console.log(tempArr)
-    // while (userList.value.length > 0) {
-    //     tempArr.push(userList.value.splice(0, 10))
-    // }
-
-//     return tempArr
-// })
 onMounted(() => {
     userAPI.getData().then(res => {
         userList.value = res
         totalNumber.value = userList.value.length
+
+
+        let tempArr = []
+        let count = Math.ceil((totalNumber.value) * 1 / 10)
+        for (let i = 0; i < count; i++) {
+            if (userList.value) {
+                tempArr.push(userList.value.splice(0, 10))
+            }
+        }
+
+        newUserList.value = tempArr
     })
 })
+
+const handleCurrentChange = (val) => {
+    currentPage.value = val - 1
+    console.log(`current page: ${val}`)
+}
 
 </script>
