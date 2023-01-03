35d6b89fe972079ed08a36a19bc1e5778f21c490
cy <cy@cy-tech.com>
2023-01-03 15:34:22 +0800

    nuxt3 naive 測試成功

--- nuxt_naive/nuxt.config.ts
+++ nuxt_naive/nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 export default defineNuxtConfig({
-
+    build: {
+        transpile: ['vueuc'],   // fix dev error: Cannot find module 'vueuc'
+    },
+    vite: {
+        plugins: [
+            Components({
+                resolvers: [NaiveUiResolver()], // Automatically register all components in the `components` directory
+            }),
+        ],
+        ssr: {
+            noExternal: ['moment', 'naive-ui', '@juggle/resize-observer', '@css-render/vue3-ssr'],
+        },
+    }
 })
---
+++ nuxt_naive2/pages/index.vue
@@ -0,0 +1,54 @@
+<script setup>
+</script>
+
+
+<template>
+    <n-space>
+        <n-button>Default</n-button>
+        <n-button type="tertiary">
+            Tertiary
+        </n-button>
+        <n-button type="primary">
+            Primary
+        </n-button>
+        <n-button type="info">
+            Info
+        </n-button>
+        <n-button type="success">
+            Success
+        </n-button>
+        <n-button type="warning">
+            Warning
+        </n-button>
+        <n-button type="error">
+            Error
+        </n-button>
+
+        <n-space>
+            <n-tag> 爱在西元前 </n-tag>
+            <n-tag type="success">
+                不该
+            </n-tag>
+            <n-tag type="warning">
+                超人不会飞
+            </n-tag>
+            <n-tag type="error">
+                手写的从前
+            </n-tag>
+            <n-tag type="info">
+                哪里都是你
+            </n-tag>
+        </n-space>
+
+        <n-result status="404" title="404 资源不存在" description="生活总归带点荒谬">
+    <template #footer>
+      <n-button>找点乐子吧</n-button>
+    </template>
+  </n-result>
+    </n-space>
+</template>
+
+
+<style scoped lang='scss'>
+
+</style>
