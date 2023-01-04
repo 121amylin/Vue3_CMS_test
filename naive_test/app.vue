4cf04b94e9caf47c91964ee14d575483977737d7
cy <cy@cy-tech.com>
2023-01-04 15:01:20 +0800

    layout

--- naive_test/app.vue
+++ naive_test/app.vue
@@ -1,5 +1,62 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="wapper">
+    <header class="header">
+      <div class="container">
+        <NuxtLink to="/">home</NuxtLink>
+        <NuxtLink to="/Validate">Validate</NuxtLink>
+        <NuxtLink to="/Avatar">Avatar</NuxtLink>
+      </div>
+    </header>
+    <main class="main">
+      <div class="container">
+        <NuxtPage />
+      </div>
+    </main>
   </div>
 </template>
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+  font-size: clamp(16px, 1vw, 18px);
+}
+
+// .wapper {
+//   display: flex;
+//   flex-direction: column;
+// }
+
+.header {
+  background-color: #eee;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+
+  a {
+    display: inline-block;
+    padding: 6px 14px;
+    margin: 6px;
+    text-decoration: none;
+    font-weight: 700;
+    color: #000;
+    transform: color .2s;
+
+    &:hover,
+    &:focus {
+      color: rgb(0, 128, 255);
+    }
+  }
+}
+
+.main {
+  margin-top: 80px;
+}
+
+.container {
+  max-width: 1280px;
+  margin: auto;
+}
+</style>
---
+++ naive_test/pages/Avatar.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+
+<template>
+    <div>
+        <n-space align="flex-end">
+            <n-avatar size="small" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+            <n-avatar size="medium" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+            <n-avatar size="large" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+            <n-avatar :size="48" src="https://07akioni.oss-cn-beijing.aliyuncs.com/07akioni.jpeg" />
+        </n-space>
+    </div>
+</template>
+
+
+<style scoped lang='scss'>
+
+</style>
