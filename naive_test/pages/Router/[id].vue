2005b2afd53defc11b0e56aa34a0a86eb5e448f0
cy <cy@cy-tech.com>
2023-01-06 10:05:33 +0800

    Nuxt 動態路由 測試

---
+++ naive_test/pages/Router/[id].vue
@@ -0,0 +1,28 @@
+<script setup>
+// const router = useRouter()
+const route = useRoute()
+</script>
+
+
+<template>
+    <div>
+        <dl>
+            <dt>router</dt>
+            <dd>template 輸出報錯：Hydration completed but contains mismatches.</dd>
+        </dl>
+        <!-- <p>{{ router }}</p> -->
+        <hr> 
+        <dl>
+            <dt>route</dt>
+            <dd>可 template 輸出</dd>
+        </dl>
+        <p>{{ route }}</p>
+        <p>route.params____{{ route.params }}</p>
+        <p>route.params.id____{{ route.params.id }}</p>
+    </div>
+</template>
+
+
+<style scoped lang='scss'>
+
+</style>
